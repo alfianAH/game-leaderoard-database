fa6b6c1c8c7f2ccc9db815a97699b31977ba6ee5
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-05 17:25:09 +0800

    Make landing page

---
+++ landing-page.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <title>Landing Page</title>
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+</head>
+
+<body>
+<main>
+    <div class="form">
+        <p>Welcome</p>
+        <button type="button" id="login-btn" onclick="">Log in</button>
+        <button type="button" id="sign-up-btn" onclick="">Sign up</button>
+    </div>
+</main>
+</body>
+</html>
